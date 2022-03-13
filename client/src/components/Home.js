ca8b0d5bc9cf01fc99a8fb837e715158a2d72814
LipikaBaniya89 <lipikabania9@gmail.com>
2022-03-14 00:12:34 +0700

    Update is not working

---
+++ client/src/components/Home.js
@@ -0,0 +1,14 @@
+// import { Link } from 'react-router-dom'
+
+// const Home = () => {
+
+//     // return (
+//     //     <div>
+//     //         MovieBloom
+//     //     </div>
+//     // )
+
+// }
+
+// export default Home
+
